6ad57847f2182fd8c0696ac06690a2c9bd7c3642
Tabita Filimon <tabitafilimon@gmail.com>
2019-02-05 20:27:48 -0500

    main tabs working

--- JS/header.js
+++ JS/header.js
@@ -1,16 +1,20 @@
 class Login {
     constructor(login){
         this.login = login;
-        
-        this.button = document.querySelector('.login');
-
-        this.button.addEventListener('click', () => this.toggleLogIn());
+        console.log(login)
+        this.login.addEventListener('mouseover', () => this.toggleLogIn());
+        this.button = document.querySelector('.logins');
+        this.content = document.querySelector('.login-content');
+        //this.button.addEventListener('mouseover', () => this.toggleLogIn());
     }
 
      toggleLogIn(){
-         this.login.classList.toggle('login-open');
+         console.log(this)
+        // const tabs = document.getElementsByClassName('active-tab');
+        // Array.from(tabs).forEach(tab => tab.classList.remove('active-tab'));
+        // this.login.classList.add('active-tab');
+        // this.login.classList.toggle('login-content');
      }
 }
-
-
-let logins = document.querySelectorAll('.logins').forEach(login => new Login(login));
+//let logins = document.querySelectorAll('.logins').forEach(login => new Login(login));
+let adminsMenu = document.querySelectorAll('.admins').forEach(login => new Login(login));
--- JS/main.js
+++ JS/main.js
@@ -1,20 +1,23 @@
 class Content{
     constructor(content){
         this.content = content
-
         this.logoButton = content.querySelector('.logoButton');
-
         this.logoButton.addEventListener('click', () => this.expandContent());
 
     }
 
     expandContent() {
-        this.content.classList.toggle('content-open');
+        // const buttons = document.getElementsByClassName('logoButton');
+        // Array.from(buttons).forEach(button => button.classList.remove('active-button'));
+        // this.content.classList.toggle('active-button');
+
+        // const words = document.getElementsByClassName('dropdown-content');
+        // Array.from(words).forEach(word => word.classList.remove('content-open'));
+        // words.classList.toggle('content-open');
+
+        // this.content.classList.toggle('content-open');
+        const p = this.content.querySelector('.dropdown-content');
+        p.classList.toggle('content-open');
     }
 }
-
-
-
-
-
 let contents = document.querySelectorAll('.content').forEach(content => new Content(content));
