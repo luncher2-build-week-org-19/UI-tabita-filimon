f5ee099f8cf9f2104187611783ddee25536c21ac
Tabita Filimon <tabitafilimon@gmail.com>
2019-02-07 14:33:00 -0500

    polished up files and finished the README file

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,17 @@
 class Content{
     constructor(content){
-        this.content = content
-        this.logoButton = content.querySelector('.logoButton');
-        this.logoButton.addEventListener('click', () => this.expandContent());
+        this.content = content;
 
+        this.mainButton = content.querySelector('.logoButton');
+
+        this.mainButton.addEventListener('click', () => this.expandContent());
     }
 
     expandContent() {
         const p = this.content.querySelector('.dropdown-content');
+
         p.classList.toggle('content-open');
     }
 }
+
 let contents = document.querySelectorAll('.content').forEach(content => new Content(content));
--- JS/news.js
+++ JS/news.js
@@ -1,16 +1,16 @@
-
 class Article {
     constructor (article){
         this.article = article;
-        console.log(this.article);
-        this.button = article.querySelector('.button');
-        console.log(this.button);
-        this.button.addEventListener('click', () => this.expandArticle());   
+        
+        this.newsButton = article.querySelector('.button');
+        
+        this.newsButton.addEventListener('click', () => this.expandArticle());   
     }
 
     expandArticle(){
-        const p = this.article.querySelector('.para');
-        p.classList.toggle('open');
+        const para = this.article.querySelector('.para');
+
+        para.classList.toggle('open');
     }
 }
 
